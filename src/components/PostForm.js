5882071c43ab36384302d24528a1e5e2d591a609
Alexandr Borodinov <yaborodinov@gmail.com>
2022-05-11 19:23:45 +0300

    added redux react-redux, added changeInputHandler

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -4,15 +4,44 @@ export default class PostForm extends React.Component {
     constructor(props) {
         super(props)
 
-        this.state = {}
+        this.state = {
+            title: ''
+        }
     }
 
+    handlerSubmit = (e) => {
+        e.preventDefault()
+
+        const {title} = this.state
+
+        const post = {
+            title,
+            id: Date.now().toString()
+        }
+        console.log(post);
+        this.setState({title: ''})
+
+    }
+
+    changeInputHandler = (e) => (
+        this.setState(prev => ({...prev, ...{
+            [e.target.name]: e.target.value
+        }}))
+    )
     render() {
         return(
-            <form>
+            <form onSubmit={this.handlerSubmit}>
                 <div className="form-group">
                     <label className='mb-2' htmlFor="title">Post Title</label>
-                    <input type="text" className="form-control mb-2" id="title" placeholder="Type..."/>
+                    <input
+                        type="text"
+                        className="form-control mb-2"
+                        id="title"
+                        placeholder="Type..."
+                        name='title'
+                        value={this.state.title}
+                        onChange={this.changeInputHandler}
+                    />
                 </div>
                 <button className="btn btn-success" type='submit'>Create</button>
             </form>
