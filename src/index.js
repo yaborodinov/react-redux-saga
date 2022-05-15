23e1b1bdc752393472f7dd5c926d0c5575731305
Alexandr Borodinov <yaborodinov@gmail.com>
2022-05-15 12:12:31 +0300

    added store, devtools extention

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import {configureStore} from '@reduxjs/toolkit';
+import {compose} from 'redux';
+import { rootReducer } from './redux/rootReducer';
+import { Provider } from 'react-redux';
+
+const store = configureStore({reducer: rootReducer}, compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
---
+++ src/redux/postsReducer.js
@@ -0,0 +1,8 @@
+const initialState = {
+    posts: [1,2,4,55],
+    fetchedPosts: []
+}
+
+export const postsReducer = (state = initialState, action) => {
+    return state
+}
