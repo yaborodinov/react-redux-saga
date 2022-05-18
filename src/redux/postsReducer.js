da4b5fd49ceea6610c3644d4387666812f046196
Alexandr Borodinov <yaborodinov@gmail.com>
2022-05-18 14:02:50 +0300

    added case for fetched posts in reducer

--- src/redux/postsReducer.js
+++ src/redux/postsReducer.js
@@ -9,6 +9,8 @@ export const postsReducer = (state = initialState, action) => {
     switch (action.type){                                              // пишем кейс в редьюсере
         case CREATE_POST:
             return  {...state, posts: [...state.posts, action.payload]}     // в action.payload мы присваивали значение post в экшн криейторе createPost 
+        case FETCHED_POSTS :
+            return {...state, fetchedPosts: action.payload}              // асинхронные посты взятые из сервера просто будем вставлять
         default: return state
     }
 }
--- src/redux/types.js
+++ src/redux/types.js
@@ -1,1 +1,6 @@
 export const CREATE_POST = 'POST/CREATE_POST'
+export const FETCHED_POSTS = 'POST/FETCHED_POSTS'
+
+// понадобится показывать состояние Loader-a  (индикатор загрузки пока идет работа с сервером)
+export const SHOW_LOADER = 'APP/SHOW_LOADER'
+export const HIDE_LOADER = 'APP/HIDE_LOADER'
