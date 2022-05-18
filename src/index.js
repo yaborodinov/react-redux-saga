27c44e47e1f26c80c94d52d4d4475d04c6a3b497
Alexandr Borodinov <yaborodinov@gmail.com>
2022-05-18 13:48:18 +0300

    installed thunk, applied it

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 import {configureStore} from '@reduxjs/toolkit';
-import {compose} from 'redux';
-import { rootReducer } from './redux/rootReducer';
+import {compose, applyMiddleware} from 'redux';   // applyMiddleware в классическом redux позволяет подключать middleware
 import { Provider } from 'react-redux';
+import thunk from 'redux-thunk'
 
-const store = configureStore({reducer: rootReducer}, compose(
+import App from './App';
+import { rootReducer } from './redux/rootReducer';
+
+const store = configureStore({reducer: rootReducer}, compose(  // configureStore взято из @reduxjs/toolkit (не классический redux)
+  applyMiddleware(                                             // подключаем applyMiddleware
+      thunk                                                    // здесь можем перечислять все middleware которые будем использовать
+  ),
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 ))
 
