47365b935dc3ef74251d1bcf6ba28e6e8f25e060
Alexandr Borodinov <yaborodinov@gmail.com>
2022-05-19 10:30:39 +0300

    added loader

--- src/components/FetchedPosts.js
+++ src/components/FetchedPosts.js
@@ -7,6 +7,9 @@ const FetchedPosts = () => {
 
 	const dispatch = useDispatch()    // диспатч с помощбю хука
 	const posts = useSelector(state => state.posts.fetchedPosts)       // с помощью этого хука достаем из store посты
+	const loader = useSelector(state => state.app.loading)             // достаем значение loading из state из редьюсера app
+
+	console.log(loader);
 	return(
 		<div className='text-center mt-3'>
 			{posts.length === 0 ?
@@ -15,6 +18,11 @@ const FetchedPosts = () => {
 			onClick={() => dispatch(fetchPosts())}          // в диспатч передаем экшн
 			>Fetch posts</button> :
 			posts.map((post) => <Post title={post.title} key={post.id}/>)}
+			{loader && 
+			<div className="spinner-border text-warning d-block mx-auto my-4" role="status">
+				<span className="sr-only"></span>
+		  	</div>
+			} 
 		</div>
 	)
 }
--- src/redux/actions.js
+++ src/redux/actions.js
@@ -1,5 +1,5 @@
 // import { createAction } from '@reduxjs/toolkit'
-import { CREATE_POST, FETCHED_POSTS } from './types'
+import { CREATE_POST, FETCHED_POSTS, HIDE_LOADER, SHOW_LOADER } from './types'
 
 export const createPost = (post) => {      // action creator
     return {
@@ -8,14 +8,28 @@ export const createPost = (post) => {      // action creator
     }
 }
 
+export const showLoader = () => {
+    return {
+        type: SHOW_LOADER
+    }
+}
+
+export const hideLoader = () => {
+    return {
+        type: HIDE_LOADER
+    }
+}
+
 export const fetchPosts = () => {
     return async dispatch => {
+        dispatch(showLoader())
         const response = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=5')
         const posts = await response.json()
         dispatch({
             type: FETCHED_POSTS,
             payload: posts
         })
+        dispatch(hideLoader())
     }
     
 }
